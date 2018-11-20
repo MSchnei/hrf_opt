adeb421abd2432bb66c9b97c9f5ff6087a1acb69
MSchnei <marian.schneider@maastrichtuniversity.nl>
2018-11-20 13:41:15 +0100

    Version bump. - Update dependency to pyprf-feature 1.1.1

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(name='hrf_opt',
-      version='1.0.1',
+      version='1.0.2',
       description=('Optimize hemodynamic response function parameters.'),
       url='https://github.com/MSchnei/hrf_opt',
       author='Marian Schneider',
       author_email='marian.schneider@maastrichtuniversity.nl',
       license='GNU General Public License Version 3',
       install_requires=['numpy', 'scipy', 'nibabel', 'cython==0.27.1',
-                        'scikit-learn==0.19.1', 'pyprf_feature==1.1.0'],
+                        'scikit-learn==0.19.1', 'pyprf_feature==1.1.1'],
       keywords=['pRF', 'fMRI', 'retinotopy'],
       long_description=long_description,
       packages=['hrf_opt'],
